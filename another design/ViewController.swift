deb49145ff058c069c45ce9574c14be002ea10eb
jonas <filerjo@quincy.edu>
2021-07-22 13:23:04 -0500

    shopping
    shopping

--- another design/ViewController.swift
+++ another design/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var shoppinglist = ["eggs", "butter", "bacon", "pancakes" ]
+    @IBOutlet weak var shoppingListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        shoppingListTableView.delegate = self
+        shoppingListTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppinglist.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = shoppinglist[indexPath.row]
+        return cell!
+    }
 }
 
